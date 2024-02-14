8d3a1a7bf8c6c67b8e7891e92806eae1f7e368a0
Ruhomaun Khaleelullah Ahmad Raza Qadri <105217847+Cxde02@users.noreply.github.com>
2024-02-14 20:17:21 +0400

    Added file

--- readdb.py
+++ readdb.py
@@ -78,9 +78,9 @@ def readConstraint():
          column_widths[i] = max(column_widths[i], len(str(value)))
 
  #Print the table header
- print("+" + "+".join("-" * (width + 2) for width in column_widths) + "+")
+ print("+" + "+".join("-" * (width) for width in column_widths) + "+")
  print("|" + "|".join(f" {column.ljust(column_widths[i])} " for i, column in enumerate(columns)) + "|")
- print("+" + "+".join("-" * (width + 2) for width in column_widths) + "+")
+ print("+" + "+".join("-" * (width) for width in column_widths) + "+")
 
  # Print the rows
  for row in rows:
@@ -99,9 +99,23 @@ def readConstraint():
  # Close the connection
  conn.close()
 
+def deleteRow():
+ # Connect to the database
+ conn = sqlite3.connect('db/moviesWatchlist.db')
+ cursor = conn.cursor()
+
+ # Execute a query to select specific rows from a table
+ cursor.execute('DELETE FROM Movies WHERE Title = "Spider-Man: Across The Spiderverse"')
+ conn.commit()
+
+ # Close the connection
+ conn.close()
+ print('Row deleted successfully!')
+
 
 
 readAll()
 #readConstraint()
+#deleteRow()
 
 
